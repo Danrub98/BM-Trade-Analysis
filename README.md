56be0a174a25572017f46e44beb7d7b080a07eb8
APB98 <borja.basolutions@gmail.com>
2021-04-28 18:32:28 -0500

    Changes to js file and html local storage and design

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # BM-Trade-Analysis
+
+## User Story
+```
+AS A Trader
+I WANT to know the exchange rate from USD/MXN and be able to
+see the news that impact the exchange rate
+SO THAT I can make informed trading decisions
+```
+Page functionality
+```
+GIVEN a get a dashboard with information from the exchange rate from USD/MXN
+WHEN I use the search bar to find important news
+THEN I am presented with the news
+WHEN I see the table where the actual exchange rate is shown
+THEN I get a real time exchange rate
+WHEN I see the historical exchange rate
+THEN I get a daily update of how the exchange rate changed
+WHEN I see the relevant news that impact the exchange rate
+THEN I get a table with news from different countries
+WHEN I am presented with a daily chart of how the exchange rate fluctuate
+THEN I get a real time graph of the USD/MXN
+```
+Why the page was created?
+```
+This webpage was created with the idea of helping traders to make informed decisions while operating in the FOREX market. Most of the times, when trying to look for information of the exchange rate USD/MXN, traders face the problem that the information is incomplete or hard to find. Mainly because the main trade information webpages focus on bigger markets, and since the Mexican market is not that huge, it doesn't has that visibility.
+```
+Problem solved
+```
+By gathering this trading information from different API's and widgets, the trader will have a more efficient access to this data, making the trading decisions more informed and all of this can be found in an user-friendly webpage.
+```
+Code used
+```
+The functionality of the page was made mainly with JavaScript, we gather the information with "fetch" from two different API's. One API was used to have a realtime exchange rate, but it can only be updated every 3 minutes, because we were only able to use a free version. The second API was used to gather the historical exchange rate, this API get's updated daily, adding new rows to the webpage each day.
+The CSS-Framework used was "Foundation" we took a header, a table format and a contact form from this framework, this made the responsive coding more easier to implement.
+```
+Image from the webpage
+![The BM trade Analysis gives information to the User abput the trade news of the USD/MXN, and an updated exchange rate.](./assets/CSS/BM.png)
--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,19 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 
-//Function to get the API polygon for the charts
+//Function to get the API polygon for the historical exchange rates of the usdmxn
 //Real time API link
 var currentDate = moment(displayTime()).format('YYYY-MM-DD');
 console.log(currentDate);
 
-var liveCharts = 'https://api.polygon.io/v2/aggs/ticker/C:MXNUSD/range/1/day/2021-04-01/' + currentDate + '?unadjusted=true&sort=asc&limit=400&apiKey=H7QtCsKuVD7PIPFbgUJjIXjtWhwPj6kS'
+var initialDate;
+if (localStorage.getItem("userDates")==""){
+    initialDate = moment(displayTime()).format('YYYY-MM-DD')
+} else {
+    initialDate = localStorage.getItem("userDates")
+};
+
+var liveCharts = 'https://api.polygon.io/v2/aggs/ticker/C:MXNUSD/range/1/day/'+ initialDate +'/' + currentDate + '?unadjusted=true&sort=asc&limit=400&apiKey=H7QtCsKuVD7PIPFbgUJjIXjtWhwPj6kS'
 console.log(liveCharts);
 
 function searchApi() {
@@ -25,6 +32,25 @@ function searchApi() {
                     console.log(data);
 
                     var table = document.getElementById("mxnusd");
+                    table.innerHTML = "";
+
+                    var ROW = document.createElement("tr");
+                    table.appendChild(ROW);
+                    var date = document.createElement("th");
+                    date.textContent = "Date";
+                    ROW.appendChild(date);
+                    var openPrice = document.createElement("th");
+                    openPrice.textContent = "Opening price";
+                    ROW.appendChild(openPrice);
+                    var closePrice = document.createElement("th");
+                    closePrice.textContent = "Closing price";
+                    ROW.appendChild(closePrice);
+                    var highestPrice = document.createElement("th");
+                    highestPrice.textContent = "Highest price";
+                    ROW.appendChild(highestPrice);
+                    var lowestPrice = document.createElement("th");
+                    lowestPrice.textContent = "Lowest price";
+                    ROW.appendChild(lowestPrice);
 
                     // Loop through the data for the lenght of result
                     for (var i = 0; i < data.results.length; i++) {
@@ -41,11 +67,11 @@ function searchApi() {
                         ROW.appendChild(closePrice);
                         var highestPrice = document.createElement("td");
                         highestPrice.textContent = data.results[i].h.toFixed(5)
-                        ROW.appendChild(highestPrice); 
+                        ROW.appendChild(highestPrice);
                         var lowestPrice = document.createElement("td");
                         lowestPrice.textContent = data.results[i].l.toFixed(5)
                         ROW.appendChild(lowestPrice);
-                        
+
                     };
                     // Print an appended table rows
 
@@ -59,36 +85,55 @@ function searchApi() {
 //Call the function
 searchApi();
 
-
+//Actual exchange rate
 var exchangeApi = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=MXN&apikey=NUMLSX6XBZRICN82.";
-function exchangeAPI(){
+function exchangeAPI() {
     fetch(exchangeApi)
-        .then(function (response){
-            if (response.ok){
-                response.json().then(function (data){
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
                     console.log(data)
                     //Convert string to float and then round.
                     console.log(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]);
                     console.log(data["Realtime Currency Exchange Rate"]["8. Bid Price"]);
-                    console.log(data["Realtime Currency Exchange Rate"]["9. Ask Price"]);          
+                    console.log(data["Realtime Currency Exchange Rate"]["9. Ask Price"]);
                     table2.textContent = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
                     table3.textContent = data["Realtime Currency Exchange Rate"]["8. Bid Price"];
                     table4.textContent = data["Realtime Currency Exchange Rate"]["9. Ask Price"];
                     return data;
 
-                        
+
                 });
             }
         })
-    };
-    var tableExhange = document.getElementById('tableExhange')
-    var table1= document.getElementById('table1')
-    var table2= document.getElementById('table2')
-    var table3= document.getElementById('table3')
-    var table4= document.getElementById('table4')
+};
+var tableExhange = document.getElementById('tableExhange');
+var table1 = document.getElementById('table1');
+var table2 = document.getElementById('table2');
+var table3 = document.getElementById('table3');
+var table4 = document.getElementById('table4');
 
 
-    
+
 exchangeAPI();
- setInterval(exchangeAPI, 180000);
-  
+setInterval(exchangeAPI, 180000);
+
+//We want to write a function that allows the user to search an specific exchange rate of a given day
+
+var SubmitDate = document.getElementById("submit-date");
+var InputDate =document.getElementById("date-input");
+SubmitDate.addEventListener("click", submitDateFx)
+function submitDateFx(event){
+    event.preventDefault
+    initialDate = InputDate.value.trim().toUpperCase();
+    localStorage.setItem("userDates", initialDate);
+    
+    liveCharts = 'https://api.polygon.io/v2/aggs/ticker/C:MXNUSD/range/1/day/'+ initialDate +'/' + currentDate + '?unadjusted=true&sort=asc&limit=400&apiKey=H7QtCsKuVD7PIPFbgUJjIXjtWhwPj6kS'
+    console.log(liveCharts)
+    searchApi();
+}
+
+//Local storage
+
+
+
